ac8ea8ae61ce06db28630fdc1243345815d8f919
yangemaster <hassanyange@gmail.com>
2023-05-13 22:38:19 +0300

    creation of models, superuser and views

--- webtech/admin.py
+++ webtech/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Portifolio_detail
 
 # Register your models here.
+
+admin.site.register(Portifolio_detail)
--- webtech/models.py
+++ webtech/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Portifolio_detail(models.Model):
+    project_name = models.CharField(max_length=255)
+    project_image =models.ImageField(upload_to='img/')
+    category = models.CharField(max_length=255)
+    client = models.CharField(max_length=255)
+    project_Date = models.DateField(auto_now_add=True)
+    project_url = models.URLField(blank=True)
+    project_Info = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.project_name
+    
