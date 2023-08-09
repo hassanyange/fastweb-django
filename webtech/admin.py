9a76bb3d5a54f0ca9280a3b30b59c4bd518b8730
yangemaster <hassanyange@gmail.com>
2023-08-09 09:26:19 +0300

    some changes

--- webtech/admin.py
+++ webtech/admin.py
@@ -7,3 +7,7 @@ admin.site.register(Portifolio_detail)
 
 admin.site.register(Portifolio_section)
 admin.site.register(Contact)
+
+admin.site.site_header ="Fastwebtech Administration"
+admin.site.site_title ="Fastwebtech Administration"
+admin.site.index_title ="Welcome to Fastwebtech"
